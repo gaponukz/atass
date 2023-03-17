1565cfe964e47e405a5bad11e717541f72fbc137
gaponukz <gaponukz54@gmail.com>
2023-03-17 14:43:00 +0200

    Add some base routes

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 from fastapi import FastAPI
-from logic.entities import User
+
+import logic.functions
+import logic.entities
+from db.json_route_db import JsonRouteDataBase
 
 app = FastAPI() # uvicorn server:app --reload
+db = JsonRouteDataBase()
 
-@app.get("/get_user")
-def read_root(user_id: str) -> str:
-    return {
-        "id" : user_id,
-        "user": User(
-            first_name = "Adam",
-            last_name = "And",
-            phone_number = "38063856332",
-            email_address = "user@gmail.com"
-        )
-    }
+@app.get("/get_unique_routes")
+def unique_routes_page() -> dict[str, logic.entities.ShortRoute]:
+    return logic.functions.get_unique_routes(db.get_all())
 
+@app.get("/get_routes_family")
+def get_routes_family_page(move_from_city: str, move_to_city: str) -> list[logic.entities.Route]:
+    return db.get_all(lambda route: route.move_from.place.city == move_from_city and route.move_to.place.city == move_to_city)
+
+@app.get("get_route")
+def get_route_page(route_id: logic.entities.HashId) -> logic.entities.Route:
+    return db.get_one(route_id)
