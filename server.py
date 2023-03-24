b5a02c05457cf562b09204f2460363eb8174d439
gaponukz <gaponukz54@gmail.com>
2023-03-24 16:39:55 +0300

    Add service/db dependency injection

--- server.py
+++ server.py
@@ -1,32 +1,46 @@
 import uvicorn
-import fastapi
 
 import logic.functions
 import logic.entities
 import logic.errors
 import authentication
 
+from fastapi import Depends
+from fastapi import FastAPI
+
 from loguru import logger
 from webtyping import Response
 
-app = fastapi.FastAPI()
+app = FastAPI()
 logger.add("logging/server.log")
 
+@logger.catch
 @app.get("/get_unique_routes")
-@logger.catch
-async def unique_routes_page() -> Response[list[logic.entities.ShortRoute]]:
-    return Response(body = await logic.functions.get_unique_routes())
+async def unique_routes_page(
+        service: logic.functions.Service = Depends()
+    ) -> Response[list[logic.entities.ShortRoute]]:
+    
+    return Response(body = await service.get_unique_routes())
 
-@app.get("/get_routes_family", dependencies=[fastapi.Depends(authentication.admin_required)])
 @logger.catch
-async def get_routes_family_page(move_from_city: str, move_to_city: str) -> Response[list[logic.entities.Route]]:
-    return Response(body = await logic.functions.get_routes_family_by_cities(move_from_city, move_to_city))
+@app.get("/get_routes_family", dependencies=[Depends(authentication.admin_required)])
+async def get_routes_family_page(
+        move_from_city: str,
+        move_to_city: str,
+        service: logic.functions.Service = Depends()
+    ) -> Response[list[logic.entities.Route]]:
+    
+    return Response(body = await service.get_routes_family_by_cities(move_from_city, move_to_city))
 
-@app.get("/get_route", dependencies=[fastapi.Depends(authentication.admin_required)])
 @logger.catch
-async def get_route_page(route_id: logic.entities.HashId) -> Response[logic.entities.Route]:
+@app.get("/get_route", dependencies=[Depends(authentication.admin_required)])
+async def get_route_page(
+        route_id: logic.entities.HashId,
+        service: logic.functions.Service = Depends()
+    ) -> Response[logic.entities.Route]:
+    
     try:
-        return Response(body = await logic.functions.get_route_by_id(route_id))
+        return Response(body = await service.get_route_by_id(route_id))
     
     except logic.errors.RouteNotFoundError as error:
         return Response(
