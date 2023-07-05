32330fd30e17d484aa0eb63c8b3a50b8946870b1
Vlad Skrynyk <skrynyk.vlad@gmail.com>
2023-07-05 21:11:49 +0300

    sign up

--- admin/src/components/SignUp.jsx
+++ admin/src/components/SignUp.jsx
@@ -1,67 +1,158 @@
-import { Button } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux"
+import { useState } from "react";
+import { fetchSignUp, fetchSignUpConfirm } from "../features/postSignUp/postSignUp";
 
-import { useSelector } from "react-redux";
+import { ToastContainer } from 'react-toastify';
+
 
 const SignUp = () => {
+    const postStatus = useSelector((state) => state.signup.status);
+    const flagSuccess = useSelector((state) => state.signup.flagSuccess)
+    const finalFlagSuccess = useSelector((state) => state.signup.finalFlagSuccess)
+    console.log(postStatus);
 
+    const dispatch = useDispatch();
+
+
+    // ui state
+    const [name, setName] = useState("");
+    const [gmail, setGmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [password, setPassword] = useState("");
+    const [confPassword, setConfPassword] = useState();
+    const [code, setCode] = useState("");
+    const [allow, setAllow] = useState("");
+
+
+    const handleButtonClickFirst = () => {
+        dispatch(fetchSignUp({ url: "signup", gmail: gmail }))
+    }
+
+    const handleButtonClickSecond = () => {
+        dispatch(fetchSignUpConfirm({ url: "confirmRegistration", gmail: gmail, password: password, fullName: name, phone: phone, allowsAdvertisement: allow, key: code }))
+    }
 
     return (
         <>
-            {/* <div
-                style={{
-                    marginLeft: "300px",
-                    marginRight: "300px"
-                }}
-            >
-                <input
-                    type="text"
-                    className="form-control rrr"
-                    placeholder="gmail"
-                />
+            <ToastContainer />
+            <div className="container">
+                <div className="input-group mb-3 " >
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Ім'я"
+                        aria-label="Recipient's username"
+                        aria-describedby="button-addon2"
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value)
+                        }}
+                    />
+                </div>
+                <div className="input-group mb-3 ">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Пошта"
+                        aria-label="Recipient's username"
+                        aria-describedby="button-addon2"
+                        value={gmail}
+                        onChange={(e) => {
+                            setGmail(e.target.value)
+                        }}
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="телефон"
+                        aria-label="Recipient's username"
+                        aria-describedby="button-addon2"
+                        value={phone}
+                        onChange={(e) => {
+                            setPhone(e.target.value)
+                        }}
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Пароль"
+                        aria-label="Recipient's username"
+                        aria-describedby="button-addon2"
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                    />
+                </div>
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Підтвердити пароль"
+                        aria-label="Recipient's username"
+                        aria-describedby="button-addon2"
+                        value={confPassword}
+                        onChange={(e) => {
+                            setConfPassword(e.target.value)
+                        }}
+                    />
+                </div>
 
-                <input
-                    type="text"
-                    className="form-control rrr"
-                    placeholder="code"
-                />
+                { (flagSuccess) &&
+                    (<div className="input-group mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Код з пошти"
+                            aria-label="Recipient's username"
+                            aria-describedby="button-addon2"
+                            value={code}
+                            onChange={(e) => {
+                                setCode(e.target.value)
+                            }}
+                        />
+                    </div>)
+                }
 
-                <Button
-                    variant="contained"
-                    onClick={() => {
-                        console.log("test");
-                    }}
-                >Click</Button>
-            </div> */}
-            <div class="container">
-                <div class="input-group mb-3 " >
-                    <input type="text" class="form-control" placeholder="Ім'я" aria-label="Recipient's username" aria-describedby="button-addon2" />
-
-                </div>
-                <div class="input-group mb-3 ">
-                    <input type="text" class="form-control" placeholder="Пошта" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                </div>
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="телефон" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                </div>
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Пароль" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                </div>
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Підтвердити пароль" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                </div>
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Код з пошти" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                </div>
                 <div>
-                    <p class="object">Дозволити надсилати рекламу на почту</p>
-                    <input type="checkbox" id="myCheckbox" class="object" />
+                    <p className="object">Дозволити надсилати рекламу на почту</p>
+                    <input 
+                        type="checkbox" 
+                        id="myCheckbox" 
+                        className="object" 
+                        checked={allow}
+                        onChange={() => {
+                            (allow) ? setAllow(false) : setAllow(true);
+
+                        }}
+                    />
                 </div>
             </div>
 
-            <div class="kn">
-                <button type="submit" class="btn" style={{ backgroundColor: "#40ABCF", color: "white", fontWeight: "bold" }} id="knop">
+            <div className="kn">
+                { (flagSuccess) ? (
+                <button 
+                    className="btn" 
+                    style={{ backgroundColor: "#40ABCF", color: "white", fontWeight: "bold" }} 
+                    id="knop"
+                    onClick={handleButtonClickSecond}
+                >
                     <span>Зареєструвати</span>
-                </button>
+                </button>) : (
+                    <button 
+                        className="btn" 
+                        style={{ backgroundColor: "#40ABCF", color: "white", fontWeight: "bold" }} 
+                        id="knop"
+                        onClick={handleButtonClickFirst}
+                    >
+                        <span>Зареєструвати</span>
+                    </button>
+                    ) }
+                
             </div>
         </>
     )
--- admin/src/features/postSignUp/postSignUp.js
+++ admin/src/features/postSignUp/postSignUp.js
@@ -1,33 +1,75 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+import { toast } from 'react-toastify';
+
 
 const POSTS_URL = "http://localhost:8080";
 
 const initialState = {
-
+  status: "idle-t", // "idle" | "loading" | "succeeded" | "failed"
+  flagSuccess: false,
+  finalFlagSuccess: false,
 }
 
 export const fetchSignUp = createAsyncThunk("signup/fetchSignUp", async ({ url, gmail }) => {
-  
+  const response = await axios.post(`${POSTS_URL}/${url}`, {
+    withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    gmail: gmail // "test@knu.ua"
+  })
+  console.log(response);
+  return response.data
+})
+
+export const fetchSignUpConfirm = createAsyncThunk("signup/fetchSignUpConfirm", async ({ url, gmail, password, fullName, phone, allowsAdvertisement, key }) => {
+  const response = await axios.post(`${POSTS_URL}/${url}`, {
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },    
+    gmail: gmail,
+    password: password,
+    fullName: fullName,
+    phone: phone,
+    allowsAdvertisement: allowsAdvertisement,
+    key: key
+  })
+  console.log(response);
+  return response.data
 })
 
 export const signUpSlice = createSlice({
-    name: "signup",
-    initialState,
-    reducers: {
-        
-    },
-    extraReducers: (builder) => {
-        builder
-          .addCase(fetchSignUp.pending, (state) => {
-            console.log("?");
-          })
-          .addCase(fetchSignUp.fulfilled, (state, action) => {
-            console.log("+");
-          })
-          .addCase(fetchSignUp.rejected, (state, action) => {
-            console.log("-");
-          });
-      },
+  name: "signup",
+  initialState,
+  reducers: {
+
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSignUp.pending, (state) => {
+        console.log("?");
+      })
+      .addCase(fetchSignUp.fulfilled, (state, action) => {
+        console.log("+");
+        state.flagSuccess = true;
+        toast.info("Введіть код", { autoClose: 1500 })
+      })
+      .addCase(fetchSignUp.rejected, (state, action) => {
+        console.log("-");
+      })
+      .addCase(fetchSignUpConfirm.pending, (state) => {
+        console.log("?");
+      })
+      .addCase(fetchSignUpConfirm.fulfilled, (state, action) => {
+        console.log("+");
+      })
+      .addCase(fetchSignUpConfirm.rejected, (state, action) => {
+        console.log("-");
+      })
+  },
 })
 
 export const { } = signUpSlice.actions;
